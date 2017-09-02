b678946ffcc4bbe469ef922f41ec3f9922c0b2a0
Barrett Quan <21barrett@gmail.com>
2017-09-02 16:06:19 -0700

    surfaced router from parent using contextTypes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,20 +10,27 @@ class StorePicker extends React.Component {
     event.preventDefault();
     console.log('You Changed the URL');
     //first grab text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Gong to ${storeId}`)
     //second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
+    // Any where else
     return (
       <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name"
-        defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
+        <input type="text" required placeholder="Store Name" defaultValue={ getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
 }
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
 
 export default StorePicker;
