766538a0a95e61cc0ee99a04e5a81586200dba26
Barrett Quan <21barrett@gmail.com>
2017-09-02 16:45:57 -0700

    created fish object fron input using state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -19,7 +19,7 @@ class StorePicker extends React.Component {
   render() {
     // Any where else
     return (
-      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={ getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store</button>
