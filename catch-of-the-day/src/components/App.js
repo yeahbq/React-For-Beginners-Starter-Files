8c1a1961e80af77e5454dff93af2c8e17b8bf73a
Barrett Quan <21barrett@gmail.com>
2017-09-05 16:43:24 -0700

    loaded data into state onClick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './Header';
-import Order from './Order'
-import Inventory from './Inventory'
-
-
+import Order from './Order';
+import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+
     //getinitialState
     this.state = {
       fishes: {},
@@ -26,6 +27,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +40,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order/>
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
